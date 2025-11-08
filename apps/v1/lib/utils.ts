a55ff76a303de69cdd77237c9f4f17b257f2bc71
Pawan Kumar <pawan@appsmith.com>
2025-11-08 17:19:39 +0530

    wip

--- apps/v1/lib/utils.ts
+++ apps/v1/lib/utils.ts
@@ -1,9 +1,6 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+"use client"
 
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
-}
+export * from "@/registry/v1/lib/utils"
 
 export function absoluteUrl(path: string) {
   return `${process.env.NEXT_PUBLIC_APP_URL}${path}`
